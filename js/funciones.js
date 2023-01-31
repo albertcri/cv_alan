9751032502275eecd504054f1e3575702bad3206
Alberto Humphreys <ing.ahumphreys@gmail.com>
2023-01-31 20:27:48 -0300

    commit entrega

--- js/funciones.js
+++ js/funciones.js
@@ -1,3 +1,19 @@
-document.getElementById("boton").onclick= function()  {
-    document.getElementById("demo").innerHTML = "2975464743"
-}
+document.getElementById("boton").onclick= function() {
+    document.getElementById("demo").innerHTML="5125432167"
+    }
+    document.getElementById("botone").onclick= function() {
+        document.getElementById("email").innerHTML="alan.robinson@example.com"
+    }
+    document.getElementById("botondire").onclick= function() {
+    document.getElementById("domicilio").innerHTML="7680 Shady Ln Dr, Milan, Italia"
+    }
+    document.getElementById("columna").addEventListener("mouseover", mouseOver);
+    document.getElementById("columna").addEventListener("mouseout", mouseOut);
+    
+    function mouseOver() {
+      document.getElementById("fn").innerHTML= "Fecha de nacimiento: 21/01/1981";
+    }
+    
+    function mouseOut() {
+      document.getElementById("fn").innerHTML = "";
+    }
